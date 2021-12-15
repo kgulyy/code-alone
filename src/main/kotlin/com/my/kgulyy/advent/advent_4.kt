965e5acda045d50e4648f184d4c3d7d2e0721a39
Konstantin Gulyy <kgulyy@my.com>
2021-12-15 19:21:48 +0300

    Add template for Advent 4

---
+++ src/main/kotlin/com/my/kgulyy/advent/advent_4.kt
@@ -0,0 +1,9 @@
+package com.my.kgulyy.advent
+
+fun main() {
+    println(advent4())
+}
+
+fun advent4(): Int {
+    return 0
+}
---
+++ src/test/kotlin/com/my/kgulyy/advent/Advent4KtTest.kt
@@ -0,0 +1,12 @@
+package com.my.kgulyy.advent
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class Advent4KtTest {
+
+    @Test
+    fun smokeTest() {
+        assertThat(advent4()).isEqualTo(0)
+    }
+}
