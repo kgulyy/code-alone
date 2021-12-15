dd5e9aea7a8c90afe97a1182be8796f33fe35f8e
Konstantin Gulyy <kgulyy@my.com>
2021-12-15 19:11:25 +0300

    Add main case for Advent 2

--- src/test/kotlin/com/my/kgulyy/advent/Advent2KtTest.kt
+++ src/test/kotlin/com/my/kgulyy/advent/Advent2KtTest.kt
@@ -6,8 +6,12 @@ import org.junit.jupiter.api.Test
 internal class Advent2KtTest {
 
     @Test
-    fun test() {
-        assertThat(advent2()).isEqualTo(6)
+    fun mainTest() {
+        assertThat(advent2(945747, 130713)).isEqualTo(27711156)
     }
 
+    @Test
+    fun smokeTest() {
+        assertThat(advent2()).isEqualTo(6)
+    }
 }
