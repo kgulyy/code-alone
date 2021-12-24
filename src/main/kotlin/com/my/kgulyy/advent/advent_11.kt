a5cecbe145ab805e3ec0b41ace395b8fa2d6ca7d
Konstantin Gulyy <kgulyy@my.com>
2021-12-24 09:59:28 +0300

    Do Advent 11

--- src/main/kotlin/com/my/kgulyy/advent/advent_11.kt
+++ src/main/kotlin/com/my/kgulyy/advent/advent_11.kt
@@ -4,30 +4,52 @@ import com.my.kgulyy.util.StringUtils
 import java.util.*
 
 fun main() {
-    println(advent11("/advent_11/main.test"))
+    println(advent11("/advent_11/smoke.test"))
 }
 
 /**
  *
  */
-fun advent11(inputFilePath: String): Int {
+fun advent11(inputFilePath: String): String {
     val inputStream = StringUtils.getResourceFileAsStream(inputFilePath)
     val scanner = Scanner(inputStream)
 
     val n = scanner.nextInt()
     val matrix = Array(n) { IntArray(scanner.nextInt()) { scanner.nextInt() } }
 
-    val setOfAvailableIndexes = List(n) { it }.toSortedSet()
+    val setOfAvailableIndexes = List(n) { it }.toMutableSet()
     var indexOfNextEmptyArray = findNextIndexOfEmptyArray(matrix, -1)
-    setOfAvailableIndexes.remove(indexOfNextEmptyArray)
 
-    var resultSequence = mutableListOf(indexOfNextEmptyArray + 1)
+    val resultSequence = mutableListOf(indexOfNextEmptyArray + 1)
+    setOfAvailableIndexes.remove(indexOfNextEmptyArray)
     indexOfNextEmptyArray = findNextIndexOfEmptyArray(matrix, indexOfNextEmptyArray)
 
+    while (setOfAvailableIndexes.isNotEmpty()) {
 
+        val list = matrix.withIndex()
+            .filter { it.index < indexOfNextEmptyArray }
+            .filter { it.index in setOfAvailableIndexes }
 
-    return 0
+        var flag = false
+        for (i in list) {
+            if (canBeInclude(i.value, resultSequence)) {
+                resultSequence.add(i.index + 1)
+                setOfAvailableIndexes.remove(i.index)
+                flag = true
+                break
+            }
+        }
+
+        if (!flag) {
+            resultSequence.add(indexOfNextEmptyArray)
+            indexOfNextEmptyArray = findNextIndexOfEmptyArray(matrix, indexOfNextEmptyArray)
+        }
+    }
+
+    return resultSequence.toString()
 }
 
 fun findNextIndexOfEmptyArray(matrix: Array<IntArray>, prevIndex: Int) =
     matrix.withIndex().indexOfFirst { it.value.isEmpty() && it.index > prevIndex }
+
+fun canBeInclude(array: IntArray, sequence: List<Int>) = sequence.containsAll(array.toList())
